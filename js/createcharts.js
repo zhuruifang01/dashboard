cc788050d5c323dd75843d5fdf1a14ede55e8cbf
Your Name <you@example.com>
2017-09-24 16:44:25 +0800

    aaa

---
+++ js/createcharts.js
@@ -0,0 +1,300 @@
+  var createcharts={};//创建函数对象
+   //柱状图1的函数：
+        createcharts.chart1=function(Xdata,Ydata1,Ydata2){
+        $(function(){
+          var chart1= echarts.init(document.getElementById('chart1'));
+          var option = {
+              //backgroundColor:'rgba(178,220,245,0.4)',//背景色
+              color: ['#3da5a1'],
+              tooltip : {  //鼠标悬浮显示
+                  trigger: 'axis',
+                  axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                      type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                  }
+              },
+              title: {
+                  text:"Timing keep Trend",
+                  textStyle: {
+                      fontSize: 20,
+                      fontWeight:'normal',
+                      color:'#218be2',            //标题颜色
+                  },
+                  x:'center'
+
+              },
+              grid: {  //图表的位置
+                  left: '2%',right:'8%',
+                  bottom: '1%',top:'25%',
+                  containLabel: true
+              },
+              legend: {  //图例：
+                  x:'8%',
+                  y:'6px',
+                  data:['Team', 'Site'],
+                  textStyle: {
+                      color: '#000'
+                  },
+              },
+              xAxis : [
+                  {   splitLine:{show:false},
+                      type : 'category',
+                      name : 'x/day',
+                      data :Xdata,
+                      axisTick: {
+                          alignWithLabel: true
+                      },
+                      axisLabel: {
+                          show: true,
+                          textStyle: {
+                              color: '#218be2'   //x轴字体颜色
+                          },
+                          // interval:0
+                      },
+                      axisLine:{
+                          lineStyle:{
+                              color:'#d5d5d5',   //x轴线颜色
+                          }
+                      },
+
+                  }
+              ],
+              yAxis : [
+
+                  {   splitLine:{show:true},  //去掉网格
+                      type : 'value',
+                      name : 'y',
+                      axisLabel: {
+                          show: true,
+                          textStyle: {
+                              color: '#218be2'
+                          }
+                      },
+                      axisLine:{
+                          lineStyle:{
+                              color:'#d5d5d5',
+                          }
+                      }
+                  }
+              ],
+              series : [
+                  {
+                      name:'Team',
+                      type:'bar',
+                      barWidth: '30%',
+                      itemStyle:{
+                          normal: {
+
+                              color: function (params) {
+                                  // build a color map as your need.
+                                  var colorList = [
+                                      '#90cdf0','#90cdf0','#90cdf0','#90cdf0','#90cdf0','#90cdf0','#90cdf0'
+                                  ];
+                                  return colorList[params.dataIndex]
+                              }
+                          }
+                      },
+                      data:Ydata1
+                  },
+                  {
+                      name:'Site',
+                      type:'bar',
+                      barWidth: '30%',
+                      itemStyle:{
+                          normal: {
+
+                              color: function (params) {
+                                  // build a color map as your need.
+                                  var colorList = [
+                                      '#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92'
+                                  ];
+                                  return colorList[params.dataIndex]
+                              }
+                          }
+                      },
+                      data:Ydata2
+                  }
+
+              ]
+          };
+          chart1.setOption(option);
+        });
+       };
+  //柱状图2的函数：
+      createcharts.chart2=function(zhu2color,aa,Xdata,Ydata1){
+          $(function(){
+              var chart2= echarts.init(document.getElementById('chart2'));
+              var option = {
+                  //backgroundColor:'rgba(178,220,245,0.4)',//背景色
+                  color: ['#3da5a1'],
+                  tooltip : {  //鼠标悬浮显示
+                      trigger: 'axis',
+                      axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                          type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                      }
+                  },
+                  title: {
+                      text:"Monthly Quality&Safety Index",
+                      textStyle: {
+                          fontSize: 20,
+                          fontWeight:'normal',
+                          color:'#218be2',            //标题颜色
+                      },
+                      x:'center'
+
+                  },
+                  grid: {  //图表的位置
+                      left: '2%',right:'8%',
+                      bottom: '1%',top:'25%',
+                      containLabel: true
+                  },
+                  xAxis : [
+                      {   splitLine:{show:false},
+                          type : 'category',
+                          data :Xdata,
+                          axisTick: {
+                              alignWithLabel: true
+                          },
+                          axisLabel: {
+                              show: true,
+                              textStyle: {
+                                  color: '#218be2'   //x轴字体颜色
+                              },
+                              // interval:0
+                          },
+                          axisLine:{
+                              lineStyle:{
+                                  color:'#d5d5d5',   //x轴线颜色
+                              }
+                          },
+
+                      }
+                  ],
+                  yAxis : [
+
+                      {   splitLine:{show:true},  //去掉网格
+                          type : 'value',
+                          name : 'y/%',
+                          axisLabel: {
+                              show: true,
+                              textStyle: {
+                                  color: '#218be2'
+                              }
+                          },
+                          axisLine:{
+                              lineStyle:{
+                                  color:'#d5d5d5',
+                              }
+                          }
+                      }
+                  ],
+                  series : [
+                      {
+                          name:aa,
+                          type:'bar',
+                          barWidth: '50%',
+                          itemStyle:{
+                              normal: {
+
+                                  color: function (params) {
+                                      // build a color map as your need.
+                                      var colorList = zhu2color
+                                      return colorList[params.dataIndex]
+                                  }
+                              }
+                          },
+                          data:Ydata1
+                      }
+
+                  ]
+              };
+              chart2.setOption(option);
+          });
+      };
+
+  //折线图的函数：
+  createcharts.chart3=function(zhecolor,aa,Xdata,Ydata1){
+      $(function(){
+          var chart3= echarts.init(document.getElementById('chart3'));
+          var option = {
+              //backgroundColor:'rgba(178,220,245,0.4)',//背景色
+              color: ['#3da5a1'],
+              tooltip : {  //鼠标悬浮显示
+                  trigger: 'axis',
+                  axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                      type : 'line'        // 默认为直线，可选为：'line' | 'shadow'
+                  }
+              },
+              title: {
+                  text:"Weekly Report Submitted",
+                  textStyle: {
+                      fontSize: 20,
+                      fontWeight:'normal',
+                      color:'#218be2',            //标题颜色
+                  },
+                  x:'center'
+
+              },
+              grid: {  //图表的位置
+                  left: '2%',right:'8%',
+                  bottom: '1%',top:'25%',
+                  containLabel: true
+              },
+              xAxis : [
+                  {   splitLine:{show:true},
+                      type : 'category',
+                      data :Xdata,
+                      axisTick: {
+                          alignWithLabel: true
+                      },
+                      axisLabel: {
+                          show: true,
+                          textStyle: {
+                              color: '#218be2'   //x轴字体颜色
+                          },
+                          // interval:0
+                      },
+                      axisLine:{
+                          lineStyle:{
+                              color:'#d5d5d5',   //x轴线颜色
+                          }
+                      },
+
+                  }
+              ],
+              yAxis : [
+
+                  {   splitLine:{show:true},
+                      type : 'value',
+                      name : 'y',
+                      axisLabel: {
+                          show: true,
+                          textStyle: {
+                              color: '#218be2'
+                          }
+                      },
+                      axisLine:{
+                          lineStyle:{
+                              color:'#d5d5d5',
+                          }
+                      }
+                  }
+              ],
+              series : [
+                  {
+                      name:aa,
+                      type:'line',
+                      barWidth: '50%',
+                      itemStyle:{
+                          normal: {
+                              color:zhecolor
+                          }
+                      },
+                      data:Ydata1
+                  }
+
+              ]
+          };
+          chart3.setOption(option);
+      });
+  };
+
--- js/index.js
+++ js/index.js
@@ -53,5 +53,91 @@ function showTime(){
 }
 showTime();
 $('.chose .time').text(str);
+// ================图表=========================
+//初始化：
+	//柱状图1；
+		zhu1X=[1,2,3,4,5,6,7];
+		zhu1Y1=[10,20,10,30,40,30,40];
+		zhu1Y2=[10,30,40,50,70,80,30];
+		createcharts.chart1(zhu1X,zhu1Y1,zhu1Y2);
+    //柱状图2；
+		zhu2X=[1,2,3,4,5,6,7];
+		zhu2Y1=[10,20,10,30,40,30,40];
+        zhu2color1=['#90cdf0','#90cdf0','#90cdf0','#90cdf0','#90cdf0','#90cdf0','#90cdf0'];
+		zhu2color2=['#e49557','#e49557','#e49557','#e49557','#e49557','#e49557','#e49557'];
+		zhu2color3=['#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92','#f3ce92'];
+		createcharts.chart2(zhu2color1,'SQI',zhu1X,zhu1Y1);
+    //折线图；
+		zheX=[1,2,3,4,5,6,7];
+		zheY1=[10,20,10,30,40,30,40];
+		createcharts.chart3('#218be2','PAT',zheX,zheY1);
 
-})
+   var BG_height=$('.biaoge ul li').height();
+   $('.biaoge ul li').css({'line-height':BG_height+'px'});
+
+//自动切换：
+	//柱状图2：
+		var num1=0;
+		function zhu2(){
+			num1++;
+			if(num1>=3){num1=0;}
+			$(".xuanze li").removeClass("active").eq(num1).addClass('active');
+			if(num1==1){
+				createcharts.chart2(zhu2color2,'SeQI',zhu1X,zhu1Y1);
+			}else if(num1==2){
+				createcharts.chart2(zhu2color3,'HHS',zhu1X,zhu1Y1);
+			}else{createcharts.chart2(zhu2color1,'SQI',zhu1X,zhu1Y1);}
+		}
+		var t1=setInterval(zhu2,5000);
+		//鼠标停留停止轮播：
+		$("#chart2").mouseover(function(){
+			clearInterval(t1);
+		}).mouseout(function(){
+			t1=setInterval(zhu2,5000);
+		});
+		//点击切换:
+		$(".xuanze1 li").click(function(){
+			var index=$(this).index(".xuanze1 li");
+			$(".xuanze1 li").removeClass("active").eq(index).addClass('active');
+			if(index==1){
+				createcharts.chart2(zhu2color2,'SeQI',zhu1X,zhu1Y1);
+			}else if(index==2){
+				createcharts.chart2(zhu2color3,'HHS',zhu1X,zhu1Y1);
+			}else{
+				createcharts.chart2(zhu2color1,'SQI',zhu1X,zhu1Y1);
+			}
+
+		});
+    //折线图:
+		var num2=0;
+		function zhe(){
+			num2++;
+			if(num2>=3){num2=0;}
+			$(".xuanze2 li").removeClass("active").eq(num2).addClass('active');
+			if(num2==1){
+				createcharts.chart3('#e49557','SA',zheX,zheY1);
+			}else if(num2==2){
+				createcharts.chart3('#f3ce92','FRA',zheX,zheY1);
+			}else{createcharts.chart3('#218be2','PAT',zheX,zheY1);}
+		}
+		var t2=setInterval(zhe,5000);
+		//鼠标停留停止轮播：
+		$("#chart3").mouseover(function(){
+			clearInterval(t2);
+		}).mouseout(function(){
+			t2=setInterval(zhe,5000);
+		});
+		//点击切换:
+		$(".xuanze2 li").click(function(){
+			var index=$(this).index(".xuanze2 li");
+			$(".xuanze2 li").removeClass("active").eq(index).addClass('active');
+			if(index==1){
+                createcharts.chart3('#e49557','SA',zheX,zheY1);
+			}else if(index==2){
+                createcharts.chart3('#f3ce92','FRA',zheX,zheY1);
+			}else{
+                createcharts.chart3('#218be2','PAT',zheX,zheY1);
+			}
+
+		});
+});
